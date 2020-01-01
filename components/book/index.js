67cb9b81052a5e14ea2e108bbab2da2967e587c1
wangjie <17054214@qq.com>
2020-01-01 18:49:36 +0800

    Promise all 合并多个并行请求

--- components/book/index.js
+++ components/book/index.js
@@ -22,7 +22,7 @@ Component({
     onTap(event) {
       const bid = this.properties.book.id
       wx.navigateTo({
-        url: '/pages/book-detail/book-detail?bid=${bid}',
+        url: `/pages/book-detail/book-detail?bid=${bid}`,
       })
     }
 
